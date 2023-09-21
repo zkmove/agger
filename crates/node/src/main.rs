10b1a49a43eed788269879311148644ef7a35f3d
caojiafeng <funfriendcjf@gmail.com>
2023-09-21 11:44:54 +0800

    proving dispatcher

--- crates/node/src/main.rs
+++ crates/node/src/main.rs
@@ -3,14 +3,18 @@ use std::sync::Arc;
 
 use clap::Parser;
 use futures_util::{pin_mut, StreamExt, TryFutureExt, TryStreamExt};
-use log::error;
+use log::{error};
+
+use tokio::select;
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::Receiver;
 
 use agger_contract_types::UserQuery;
-use agger_node::proving::prove;
-use agger_node::query_runner::witness;
+use agger_node::proving::{ProveTask, ProvingTaskDispatcher};
+
 use agger_node::vk_generator::VerificationParameters;
 use agger_storage::schemadb::Options;
-use agger_storage::{AggerStore, UserQueryKey, UserQuerySchema, UserQueryValue};
+use agger_storage::{UserQueryKey, UserQuerySchema, UserQueryValue};
 use aptos_events::{AggerQueryManager, AggerQueryParam, AptosAccountAddress, AptosBaseUrl};
 
 #[derive(Parser, Debug)]
@@ -27,7 +31,7 @@ struct StartServer {
     #[arg(long)]
     agger_address: AptosAccountAddress,
     /// storage path
-    #[arg(long)]
+    #[arg(long, default_value = "aggerdb")]
     store_path: Option<PathBuf>,
 }
 
@@ -43,6 +47,7 @@ fn parse_aptos_url(rpc: &str) -> anyhow::Result<AptosBaseUrl> {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    env_logger::try_init()?;
     let cli: Cli = Cli::parse();
     println!("cmd:{:?}", &cli);
     match cli {
@@ -57,18 +62,25 @@ async fn main() -> anyhow::Result<()> {
                 options.create_if_missing(true);
                 options.create_missing_column_families(true);
 
-                let db = agger_storage::schemadb::DB::open(
+                
+
+                agger_storage::schemadb::DB::open(
                     store_path.as_deref().unwrap_or(Path::new(".")),
                     "agger-db",
                     vec!["queries", "proofs"],
                     &options,
-                )?;
-
-                db
+                )?
             };
             let store = Arc::new(store);
 
-            let event_manager = aptos_events::AggerQueryManager::new(
+            let prover_threads = threadpool::Builder::new()
+                .thread_name("provers".to_string())
+                .build();
+            let (task_sender, task_receiver) = mpsc::channel(32);
+            let (output_sender, output_receiver) = mpsc::channel(32);
+            let provers = ProvingTaskDispatcher::new(prover_threads, task_receiver, output_sender);
+
+            let event_manager = AggerQueryManager::new(
                 parse_aptos_url(&aptos_rpc)?,
                 AggerQueryParam {
                     aggger_address: agger_address,
@@ -77,7 +89,7 @@ async fn main() -> anyhow::Result<()> {
             let new_query_event_stream = event_manager
                 .clone()
                 .get_query_stream()
-                .map_err(|e| anyhow::Error::new(e))
+                .map_err(anyhow::Error::new)
                 .and_then(|s| {
                     prepare_prove_data(event_manager.clone(), s.clone())
                         .map_ok(|(ms, vk)| (s, ms, vk))
@@ -85,26 +97,45 @@ async fn main() -> anyhow::Result<()> {
                 .fuse();
             pin_mut!(new_query_event_stream);
 
-            while let Some(s) = new_query_event_stream.next().await {
-                println!("{:?}", s);
-                match s {
-                    Ok((query, modules, vp)) => {
-                        store.put::<UserQuerySchema>(
-                            &UserQueryKey::from(query.sequence_number),
-                            &UserQueryValue::from(query.clone()),
-                        )?;
-
-                        let witness = witness(query.clone(), modules, &vp)?;
-                        let _proof = prove(query, witness, &vp)?;
-                        // submit proof
+            let mut dispatch_task_handle = tokio::spawn(provers.run());
+            let mut output_handle = tokio::spawn(handle_prove_output(output_receiver));
+            loop {
+                select! {
+                    _output_task_result = &mut output_handle => {
+                        // when output handle is gone, then output receiver is gone.
+                        // then dispatcher will go down.
                     }
-                    Err(e) => {
-                        error!("get query error. {:?}", e);
+                    _dispatch_task_result = &mut dispatch_task_handle => {
+                        // when dispatcher is gone, then task_sender cannot send any task,
+                        // it will go down automatically.
+                    }
+                    Some(s) = new_query_event_stream.next() => {
+                        match s {
+                            Ok((query, modules, vp)) => {
+                                store.put::<UserQuerySchema>(
+                                    &UserQueryKey::from(query.sequence_number),
+                                    &UserQueryValue::from(query.clone()),
+                                )?;
+                                let task  = ProveTask {query: query.clone(), modules, vp};
+                                if let Err(_err)  = task_sender.send(task).await {
+                                    error!("prover dispatcher is down");
+                                    break;
+                                }
+
+                            }
+                            Err(e) => {
+                                error!("get query error. {:?}", e);
+                            }
+                        }
+                    }
+                    else => {
+                        // no query events anymore
+                        break;
                     }
                 }
             }
 
-            println!("Hello, world!");
+            println!("Agger stopped!");
         }
     }
 
@@ -119,4 +150,11 @@ async fn prepare_prove_data(
     let (config, vk, param) = event_manager.get_vk_for_query(&query).await?;
     let vp = VerificationParameters::new(config, vk, param);
     Ok((modules, vp))
+}
+
+async fn handle_prove_output(mut receiver: Receiver<(UserQuery, anyhow::Result<Vec<u8>>)>) {
+    while let Some(output) = receiver.recv().await {
+        println!("prove result: {:?}", output);
+        // TODO: submit proof
+    }
 }
--- crates/node/src/proving.rs
+++ crates/node/src/proving.rs
@@ -1,14 +1,131 @@
 use anyhow::{ensure, Result};
+use futures_util::stream::{FuturesUnordered};
+use futures_util::StreamExt;
 use halo2_proofs::halo2curves::bn256::{Bn256, Fr};
 use halo2_proofs::poly::kzg::commitment::ParamsKZG;
 use halo2_proofs::SerdeFormat;
+use log::{error, info};
+use threadpool::ThreadPool;
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::oneshot;
 use zkmove_vm_circuit::circuit::VmCircuit;
 use zkmove_vm_circuit::witness::Witness;
 use zkmove_vm_circuit::{prove_vm_circuit_kzg, setup_vm_circuit};
 
 use agger_contract_types::UserQuery;
 
+use crate::query_runner::witness;
 use crate::vk_generator::VerificationParameters;
+
+#[derive(Clone, Debug)]
+pub struct ProveTask {
+    pub query: UserQuery,
+    pub modules: Vec<Vec<u8>>,
+    pub vp: VerificationParameters,
+}
+
+#[derive(Debug)]
+pub struct ProvingTaskDispatcher {
+    task_receiver: Receiver<ProveTask>,
+    output_sender: Sender<(UserQuery, Result<Vec<u8>>)>,
+    threadpool: ThreadPool,
+}
+
+impl ProvingTaskDispatcher {
+    pub fn new(
+        threadpool: ThreadPool,
+        task_receiver: Receiver<ProveTask>,
+        output_sender: Sender<(UserQuery, Result<Vec<u8>>)>,
+    ) -> Self {
+        Self {
+            output_sender,
+            threadpool,
+            task_receiver,
+        }
+    }
+    pub async fn run(mut self) {
+        let mut fs = FuturesUnordered::new();
+        loop {
+            if self.threadpool.active_count() == self.threadpool.max_count() {
+                // when ongoing tasks is full, we wait on one task finishing, then go on to receive new tasks.
+                if let Some(received_output) = fs.next().await {
+                    match received_output {
+                        Ok(output) => {
+                            if let Err(_) = self.output_sender.send(output).await {
+                                break;
+                            }
+                        }
+                        Err(_) => {
+                            error!("task ended without sending result");
+                        }
+                    }
+                }
+            }
+            let task = tokio::select! {
+                received_output = fs.next() => {
+                    match received_output {
+                        Some(Ok(output)) => {
+                            if let Err(_) = self.output_sender.send(output).await {
+                                // output receiver is gone, stop dispatching
+                                break
+                            }
+                            continue
+                        }
+                        Some(Err(_)) => {
+                            error!("task ended without sending result");
+                            continue
+                        }
+                        None => {
+                            info!("empty task queues, wait for new task");
+                            self.task_receiver.recv().await
+                        }
+                    }
+                }
+                task = self.task_receiver.recv() => {
+                    task
+                }
+            };
+            if let Some(task) = task {
+                info!(
+                    "new query task, user: {:#x}, id: {}",
+                    task.query.user, task.query.id
+                );
+                let (tx, rx) = oneshot::channel();
+                fs.push(rx);
+                self.threadpool.execute(|| {
+                    let query = task.query.clone();
+                    let output = run_task(task);
+                    if let Err(_) = tx.send((query, output)) {
+                        error!("task ended, but output receiver is lost");
+                    }
+                });
+            } else {
+                // all task sender is gone, just stop dispatching.
+                break;
+            }
+        }
+        info!("prove dispatcher is closing...");
+        // before closing, wait any queued or ongoing computations to finish.
+        while let Some(received_output) = fs.next().await {
+            match received_output {
+                Ok(output) => {
+                    if let Err(_) = self.output_sender.send(output).await {
+                        break;
+                    }
+                }
+                Err(_) => {
+                    error!("task ended without sending result");
+                }
+            }
+        }
+        info!("prove dispatcher is closed");
+    }
+}
+
+fn run_task(ProveTask { query, modules, vp }: ProveTask) -> Result<Vec<u8>> {
+    let witness = witness(query.clone(), modules, &vp)?;
+    prove(query, witness, &vp)
+}
 
 pub fn prove(
     _query: UserQuery,
